a4df8dddcd5458e9fab8d41a3a3531daf88772c7
Marco Ricci <m@the13thletter.info>
2024-06-08 19:06:56 +0200

    Fix documentation link to `sequin.Sequin`

--- src/derivepassphrase/__init__.py
+++ src/derivepassphrase/__init__.py
@@ -31,9 +31,9 @@ class Vault:
     detail][ALGORITHM] in his blog post on said topic: A principally
     infinite bit stream is obtained by running a key-derivation function
     on the master passphrase and the service name, then this bit stream
-    is fed into a [sequin][] to generate random numbers in the correct
-    range, and finally these random numbers select passphrase characters
-    until the desired length is reached.
+    is fed into a [Sequin][sequin.Sequin] to generate random numbers in
+    the correct range, and finally these random numbers select
+    passphrase characters until the desired length is reached.
 
     [vault]: https://getvau.lt
     [ALGORITHM]: https://blog.jcoglan.com/2012/07/16/designing-vaults-generator-algorithm/
